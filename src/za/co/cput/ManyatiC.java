798dd48dd4ce18a30aef63e11e7e5336a52a0efd
Manyati-2020 <manymunya@gmail.com>
2020-04-12 21:04:24 +0200

    Modified your project

---
+++ src/za/co/cput/ManyatiC.java
@@ -0,0 +1,11 @@
+package za.co.cput;
+
+public class ManyatiC {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        System.out.println( 2020 );
+        System.out.println( "Hi just Modified your project.");
+    }
+}
